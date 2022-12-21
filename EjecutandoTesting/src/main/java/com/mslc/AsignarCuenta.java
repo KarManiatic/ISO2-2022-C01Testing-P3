cdc19b3b59e258d924dd95ec42cddf152ebca437
VictorOrtegaGomez <114732916+VictorOrtegaGomez@users.noreply.github.com>
2022-12-21 16:34:39 +0100

    cambio en los if, adición de llaves

--- EjecutandoTesting/src/main/java/com/mslc/AsignarCuenta.java
+++ EjecutandoTesting/src/main/java/com/mslc/AsignarCuenta.java
@@ -22,25 +22,33 @@ public class AsignarCuenta {
 		boolean independizado = cliente.isIndependizado();
 		String cuenta;
 		
-		if(edad < 18 && estudiante && !independizado) 
+		if(edad < 18 && estudiante && !independizado){ 
 			cuenta = "Cuenta: Comfort";
+		}
 		
-		else if(edad < 25 && estudiante && independizado) 
+		else if(edad < 25 && estudiante && independizado){
 			cuenta = "Cuenta: Vamos que tu puedes";
+		}
 		
-		else if(edad > 18 && edad < 25 && !estudiante && !independizado) 
+		else if(edad > 18 && edad < 25 && !estudiante && !independizado){
 			cuenta = "Cuenta: Ahorra ahora que puedes";
+		}
 		
-		else if(edad > 18 && edad < 25 && !estudiante && independizado) 
+		else if(edad > 18 && edad < 25 && !estudiante && independizado){
 			cuenta  = "Cuenta: Saltando del nido";
+		}
 		
-		else if(edad >= 25 && !estudiante && !independizado)
+		else if(edad >= 25 && !estudiante && !independizado){
 			cuenta = "Cuenta: Independizate que va siendo hora";
+		}
 		
-		else if(edad >= 25 && !estudiante && independizado) 
+		else if(edad >= 25 && !estudiante && independizado){
 			cuenta = "Cuenta: Bienvenido a la vida adulta";
+		}
 		
-		else cuenta = "No existe cuenta bancaria que se ajuste a ese perfil";
+		else{
+			cuenta = "No existe cuenta bancaria que se ajuste a ese perfil";
+		}
 		
 		return cuenta;
 	}
